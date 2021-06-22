fd41582b9c927b7a3c7b90ac6499514ba7ca71ff
DianaSanchezordonez <dianaordonez1998@gmail.com>
2021-06-22 13:58:56 -0500

    bugs with jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,45 +10,21 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async checkPassword(
-    passwordSent: string,
-    passwordStored: string,
-  ): Promise<boolean> {
-    if (!passwordSent) {
-      throw new UnprocessableEntityException('Password cant be empty');
-    }
-    const IsPasswordMatching = await bcrypt.compare(
-      passwordSent,
-      passwordStored,
-    );
-    return IsPasswordMatching;
-  }
-
-  async validateUser(email: string, password: string) {
-    const userStored = await this.userService.findOne(email);
-    console.log(userStored);
-    // if (userStored == null) {
-    //   throw new UnprocessableEntityException('ERROR: invalid email');
-    // }
-    const passwordChecked = await this.checkPassword(
-      password,
-      userStored.password,
-    );
-    console.log('password', passwordChecked);
-    console.log('passwordChecked', passwordChecked);
-    if (userStored && passwordChecked) {
-      const { password, email, ...result } = userStored;
-      return result;
-    }
-
-    return null;
-  }
-
-  async login(user: any) {
+  
+  createToken(user) {
+    console.log('user', user)
     const payload = { username: user.useranme, id: user.id };
 
     return {
       access_token: this.jwtService.sign(payload),
     };
   }
+
+  // async login(user: any) {
+  //   const payload = { username: user.useranme, id: user.id };
+
+  //   return {
+  //     access_token: this.jwtService.sign(payload),
+  //   };
+  // }
 }
--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,16 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
+import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
+  constructor(private userService: UsersService) {
     super({ usernameField: 'email' });
   }
 
   async validate(email: string, password: string) {
-    const user = await this.authService.validateUser(email, password);
+    const user = await this.userService.signIn(email, password);
     if (!user) {
       throw new UnauthorizedException();
     }
