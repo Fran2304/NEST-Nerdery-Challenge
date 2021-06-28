c4601192f6c057911bb8b791ac5f26d23657ea10
DianaSanchezordonez <dianaordonez1998@gmail.com>
2021-06-28 16:30:03 -0500

    new changes

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: npm run start:prod
-release: npm run prisma:migrate:run
+release: npm run prisma:migrate:run && npm run prisma:seed
---
+++ prisma/seed.ts
@@ -0,0 +1,49 @@
+import { Logger } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { hashSync } from 'bcrypt';
+
+const password = hashSync('Password123', 10);
+
+const users = [
+  {
+    username: 'diana98',
+    email: 'dianaordonez1998@gmail.com',
+    password,
+  },
+  {
+    username: 'fran',
+    email: 'lilatiravanttiarteaga@gmail.com',
+    password,
+  },
+  {
+    username: 'fernando',
+    email: 'fernando@ravn.co',
+    password,
+  },
+];
+
+const prisma = new PrismaClient();
+
+async function main() {
+  for (const user of users) {
+    const { username, email, password } = user;
+    await prisma.user.create({
+      data: {
+        username,
+        email,
+        password,
+        role: 'MANAGER',
+        hashActivation: 'caracteresaleatorios',
+      },
+    });
+  }
+}
+
+main()
+  .catch((e) => {
+    Logger.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
